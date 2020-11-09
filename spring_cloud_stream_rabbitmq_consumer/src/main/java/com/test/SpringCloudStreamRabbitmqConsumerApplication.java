dca116a03226f7f90c3d0f4aa0a3b28942fa5565
ellery <allotory@msn.com>
2020-11-09 16:45:43 +0800

    feat: add spring cloud stream

---
+++ spring_cloud_stream_rabbitmq_consumer/src/main/java/com/test/SpringCloudStreamRabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamRabbitmqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamRabbitmqConsumerApplication.class, args);
+    }
+
+}
---
+++ spring_cloud_stream_rabbitmq_consumer2/src/main/java/com/test/SpringCloudStreamRabbitmqConsumer2Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamRabbitmqConsumer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamRabbitmqConsumer2Application.class, args);
+    }
+
+}
